dceeca790b332b3cf08d4eb9bc535e15535fd54b
Paul Le Grand Des Cloizeaux <paul.legranddec@gmail.com>
2018-09-25 13:35:48 +0200

    tests for forward propagation + fixes for bug revealed by these tests

--- lib/model.py
+++ lib/model.py
@@ -1,14 +1,16 @@
 import numpy as np
+import pickle
 
 
 class Model:
-    weight_matrices = []
-    values_before_activation = []
-    values_after_activation = []
 
     def __init__(self, input_size : int, layers : list):
         self.layers = layers
         self.input_size = input_size
+
+        self.weight_matrices = []
+        self.values_before_activation = []
+        self.values_after_activation = []
 
     def add_layer(self, layer):
         self.layers.append(layer)
@@ -19,7 +21,7 @@ class Model:
         self.values_after_activation.append(np.empty((self.layers[0].size, 1)))
         for precedent_layer, layer in zip(self.layers, self.layers[1:]):
             self.values_before_activation.append(np.empty((layer.size, 1)))
-            self.values_after_activation.append(np.empty((layer[0].size, 1)))
+            self.values_after_activation.append(np.empty((layer.size, 1)))
 
     def initialize_random(self):
         # not DRY ¯\_(ツ)_/¯
@@ -30,7 +32,7 @@ class Model:
             self.weight_matrices.append(
                 np.random.randn(layer.size, precedent_layer.size))
 
-    def initialize_from_wheights(self, weights_list):
+    def initialize_from_weights(self, weights_list):
         assert len(weights_list) == len(
             self.layers), "weigth list needs to be the same lenght as the number of layers"
         assert weights_list[0].shape == (
@@ -38,7 +40,7 @@ class Model:
         self._initialize_vect()
         self.weight_matrices = []
         self.weight_matrices.append(weights_list[0])
-        for weigths, precedent_layer, layer in zip(weights_list[1:], self.layers, self.layers[1:]):
+        for weights, precedent_layer, layer in zip(weights_list[1:], self.layers, self.layers[1:]):
             assert weights.shape == (
                 layer.size, precedent_layer.size), "weight matrices are not the right size"
             self.weight_matrices.append(weights)
@@ -47,7 +49,7 @@ class Model:
         # not DRY ¯\_(ツ)_/¯
         np.dot(self.weight_matrices[0], input_vec,
                out=self.values_before_activation[0])
-        layers[0].activation.function(
+        self.layers[0].activation.function(
             self.values_before_activation[0], out=self.values_after_activation[0])
 
         for layer, matrix, previous_value, before_act, after_act in zip(
--- tests.py
+++ tests.py
@@ -32,6 +32,47 @@ class TestActivation(unittest.TestCase):
         self.tanh.derivative(self.vec_in, self.vec_out)
         self.assertTrue(np.all(self.vec_out == np.array([[d_tanh(1)],[d_tanh(2)],[d_tanh(3)]])))
 
+class TestModel1(unittest.TestCase):
+    def setUp(self):
+        self.model = lib.model.Model(1,[
+            lib.layer.Layer(1, lib.activation.identity)
+        ])
+        self.model.initialize_from_weights([np.array([[1]], dtype=np.float_)])
+    
+    def testForward(self):
+        input_vec = np.array([[5]],dtype=np.float_)
+        output_vec = self.model.infer(input_vec)
+        self.assertTrue(np.all(output_vec == input_vec))
+
+class TestModel2(unittest.TestCase):
+    def setUp(self):
+        self.model = lib.model.Model(1,[
+            lib.layer.Layer(1, lib.activation.tanh),
+            lib.layer.Layer(1, lib.activation.tanh)
+        ])
+        self.model.initialize_from_weights([np.array([[1]], dtype=np.float_), np.array([[1]], dtype=np.float_)])
+    
+    def testForward(self):
+        input_vec = np.array([[5]],dtype=np.float_)
+        output_vec = self.model.infer(input_vec)
+        self.assertTrue(np.all(output_vec == np.array([[tanh(tanh(5))]],dtype=np.float_)))
+
+class TestModel3(unittest.TestCase):
+    def setUp(self):
+        self.model = lib.model.Model(2,[
+            lib.layer.Layer(2, lib.activation.identity),
+            lib.layer.Layer(1, lib.activation.identity)
+        ])
+        self.model.initialize_from_weights([
+            np.array([[1,2],[3,4]], dtype=np.float_),
+            np.array([[1,1]], dtype=np.float_)
+            ])
+    
+    def testForward(self):
+        input_vec = np.array([[1],[2]],dtype=np.float_)
+        output_vec = self.model.infer(input_vec)
+        self.assertTrue(np.all(output_vec == np.array([[16]],dtype=np.float_)))
+
 
 if __name__ == '__main__':
     unittest.main()
