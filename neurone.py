892c70a5eec60abebc357d99bcae6bea6b459a2b
Nathan Cassereau <nathan.cassereau@supelec.fr>
2018-10-01 19:23:56 +0200

    Modèle du neurone

---
+++ neurone.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import numpy as np
+from outils import *
+
+class Layer :
+
+    def __init__(self, entry, neurones, activation, diff_activation) :
+        # entry is the number of neurones of the previous layer
+        # neurones is the number of neurones of the current layer
+        # activation is the activation function of this layer
+
+        self.weights = np.ones((neurones, entry + 1)) #+1 for the threshold
+        self.f = activation
+        self.f_prim = diff_activation
+        self.activation_level = np.zeros(neurones)
+        self.output = np.zeros(neurones)
+
+    def compute(self, Input) :
+        vec = np.append(Input, np.array([-1]))
+        self.activation_level = np.dot(self.weights, vec)
+        for i in range(len(self.output)) :
+            self.output[i] = self.f(self.activation_level[i])
+
+    def __len__(self) :
+        return len(self.output)
+
+
+
+
+layer1 = Layer(2, 1, sigmoid, sigmoid_prim)
+layer1.compute([2,1])
