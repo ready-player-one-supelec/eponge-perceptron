e4c7c63eae55d5b1c7b0deecab7a8952c2646295
Nathan Cassereau <nathan.cassereau@supelec.fr>
2018-10-10 12:10:25 +0200

    Improved display : ready for real simulation

--- mnist.py
+++ mnist.py
@@ -27,13 +27,14 @@ def learning(network, training_image, training_label, times):
     for j in range(times) :
         random.shuffle(liste)
         for i,e in enumerate(liste) :
-            print("Times :", j, " - i : ", i)
+            if i % 100 == 0 :
+                print("Learning #{} -".format(times),"Times :", j, " - Data : ", i)
             t = [0] * 10
             t[int(training_label[e])] = 1
             network.learning(training_image[e].flatten(),t)
 
 
-def test_sample(network, test_image, test_label) :
+def test_sample(network, test_image, test_label,times) :
     failure = 0
     success = 0
 
@@ -41,12 +42,8 @@ def test_sample(network, test_image, test_label) :
         output = network.test(test_image[i].flatten())
         output = list(output)
         likely_number = output.index(max(output))
-        print("output:",output)
-        print("guess:",likely_number)
-        print("truth:",int(test_label[i]))
-        print("success:", success)
-        print("failures:", failure)
-        print("-------------------------")
+        if i % 100 == 0 :
+            print("Testing #{} - success :".format(times), success," - failures :", failure)
         if likely_number == int(test_label[i]) :
             success += 1
         else :
@@ -65,8 +62,8 @@ def create_network() :
 def network_testing(times, training_image, training_label, test_image, test_label, test_failure_rate, training_failure_rate) :
     network = create_network()
     learning(network, training_image, training_label, times)
-    training_success, training_failure = test_sample(network, training_image, training_label)
-    test_success, test_failure = test_sample(network, test_image, test_label)
+    training_success, training_failure = test_sample(network, training_image, training_label, times)
+    test_success, test_failure = test_sample(network, test_image, test_label, times)
     test_failure_rate.append(test_failure / (test_success + test_failure))
     training_failure_rate.append(training_failure / (training_success + training_failure))
     return network
@@ -92,7 +89,7 @@ training_image = read_idx("data/MNIST/train-images-idx3-ubyte")
 training_label = read_idx("data/MNIST/train-labels-idx1-ubyte")
 test_image = read_idx("data/MNIST/t10k-images-idx3-ubyte")
 test_label = read_idx("data/MNIST/t10k-labels-idx1-ubyte")
-N = 100 # how many times the training algorithm is applied to the training dataset
+N = 10 # how many times the training algorithm is applied to the training dataset
 
 times = [i for i in range(1,N+1)]
 test_failure_rate = []
