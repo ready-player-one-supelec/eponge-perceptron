4d3c1860fcf4ebd423f5c89edc98bcc3c40c1114
Nathan Cassereau <nathan.cassereau@supelec.fr>
2018-10-10 10:44:16 +0200

    Add 2 more functions just in case

--- outils.py
+++ outils.py
@@ -11,3 +11,11 @@ sigmoid_prim = lambda x : sigmoid(x) * (1 - sigmoid(x))
 
 linear = lambda x : x
 linear_prim = lambda x : 1
+
+def tanh(x) :
+    X = math.exp(2 * x)
+    return (X - 1) / (X + 1)
+tanh_prim = lambda x : 1 - tanh(x) ** 2
+
+reLU = lambda x : x if x >= 0 else 0
+reLU_prim = lambda x : 1 if x >=0 else 0
