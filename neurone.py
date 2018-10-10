4ac5ece5b53d37408891ecc9fffa613f30e671bd
Nathan Cassereau <nathan.cassereau@supelec.fr>
2018-10-10 10:44:55 +0200

    Weights are randomly between -1 and 1 (uniform). Can set weights

--- neurone.py
+++ neurone.py
@@ -11,8 +11,8 @@ class Layer :
         # neurones is the number of neurones of the current layer
         # activation is the activation function of this layer
 
-        self.weights = np.random.rand(neurones, entry)
-        self.bias = np.random.rand(neurones)
+        self.weights = 2 * np.random.rand(neurones, entry) - np.ones((neurones,entry))
+        self.bias = 2 * np.random.rand(neurones) - np.ones(neurones)
         self.f = activation
         self.f_prim = diff_activation
         self.activation_level = np.zeros(neurones)
@@ -29,3 +29,10 @@ class Layer :
     def update(self, delta_weights, delta_bias) :
         self.weights += delta_weights
         self.bias += delta_bias
+
+    def set_weights(self, weights, bias) :
+        for i in range(len(weights)) :
+            for j in range(len(weights[i])) :
+                self.weights[i,j] = weights[i,j]
+        for i in range(len(bias)) :
+            self.bias[i] = bias[i]
