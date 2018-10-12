Multicore calculation corrected : no useless instructions like before

mnist.py
@@ -21,29 +21,28 @@ def read_idx(filename):
         return np.fromstring(f.read(), dtype=np.uint8).reshape(shape).astype(np.float_)
 
 
-def learning(network, training_image, training_label, runs):
+def learning(network, training_image, training_label, iteration, run):
     liste = list(range(len(training_image)))
-    for j in range(runs) :
-        random.shuffle(liste)
-        for i,e in enumerate(liste) :
-            if i % 100 == 0 :
-                print("Learning #{} -".format(runs),"Times :", j, " - Data : ", i)
-            t = [0] * 10
-            t[int(training_label[e])] = 1
-            network.learning(training_image[e].flatten(),t)
+    random.shuffle(liste)
+    for i,e in enumerate(liste) :
+        if i % 1000 == 0 :
+            print("Run #{} -".format(run),"Iteration :", iteration, " - Data : ", i)
+        t = [0] * 10
+        t[int(training_label[e])] = 1
+        network.learning(training_image[e].flatten(),t)
 
 
-def test_sample(network, test_image, test_label,runs) :
+def test_sample(network, image, label,run) :
     failure = 0
     success = 0
 
-    for i in range(len(test_image)) :
-        output = network.test(test_image[i].flatten())
+    for i in range(len(image)) :
+        output = network.test(image[i].flatten())
         output = list(output)
         likely_number = output.index(max(output))
-        if i % 100 == 0 :
-            print("Testing #{} - success :".format(runs), success,"- failures :", failure)
-        if likely_number == int(test_label[i]) :
+        if i % 1000 == 0 :
+            print("Testing #{} - success :".format(run), success,"- failures :", failure)
+        if likely_number == int(label[i]) :
             success += 1
         else :
             failure += 1
@@ -51,32 +50,21 @@ def test_sample(network, test_image, test_label,runs) :
 
 
 def create_network() :
-    layer1 = Layer(784, 100, sigmoid, sigmoid_prim, 0.005)
-    layer2 = Layer(100, 50, sigmoid, sigmoid_prim, 0.005)
-    layer3 = Layer(50,10, sigmoid, sigmoid_prim, 0.005)
+    layer1 = Layer(784, 16, sigmoid, sigmoid_prim, 0.005)
+    layer2 = Layer(16, 16, sigmoid, sigmoid_prim, 0.005)
+    layer3 = Layer(16, 10, sigmoid, sigmoid_prim, 0.005)
     network = Network([layer1,layer2,layer3], normalisation = True)
     return network
 
 
-def network_testing(runs, training_image, training_label, test_image, test_label) :
-    network = create_network()
-    learning(network, training_image, training_label, runs)
-    training_success, training_failure = test_sample(network, training_image, training_label, runs)
-    test_success, test_failure = test_sample(network, test_image, test_label, runs)
-    test_failure_rate = test_failure / (test_success + test_failure)
-    training_failure_rate = training_failure / (training_success + training_failure)
-    save.save_network(network, "data/Networks Saved/best_network_times{}".format(runs))
-    return test_failure_rate, training_failure_rate
-
-
-def results(runs, training_failure_rate, test_failure_rate) :
-    plt.plot(runs,training_failure_rate,'r-', label="Taux d'échec sur l'échantillon d'apprentissage")
-    plt.plot(runs, test_failure_rate, 'b-', label="Taux d'échec sur l'échantillon test")
+def results(iterations, training_failure_rate, test_failure_rate, runs) :
+    plt.plot(iterations, training_failure_rate,'r-', label="Failure rate on the training sample")
+    plt.plot(iterations, test_failure_rate, 'b-', label="Failure rate on the test sample")
     plt.legend()
-    plt.xlabel("Nombre de passages de l'échantillon d'apprentissage")
-    plt.ylabel("Taux d'erreur")
+    plt.xlabel("Number of iterations")
+    plt.ylabel("Average failure rate (on {} runs)".format(runs))
     plt.ylim(0,1)
-    plt.title("Erreurs du réseau sur les échantillons de test et d'apprentissage")
+    plt.title("Failure rates of the network on training and test samples ({} runs)".format(runs))
     plt.savefig("data/Graph/MNIST_trans.png", transparent= True)
     plt.savefig("data/Graph/MNIST.png", transparent=False)
 
@@ -85,6 +73,10 @@ def results(runs, training_failure_rate, test_failure_rate) :
 # MAIN
 #########################
 
-def main(runs, training_image, training_label, test_image, test_label) :
-    test_failure_rate, training_failure_rate = network_testing(runs, training_image, training_label, test_image, test_label)
+def main(training_image, training_label, test_image, test_label, network, iteration, run) :
+    learning(network, training_image, training_label, iteration, run)
+    training_success, training_failure = test_sample(network, training_image, training_label, run)
+    test_success, test_failure = test_sample(network, test_image, test_label, run)
+    test_failure_rate = test_failure / (test_success + test_failure)
+    training_failure_rate = training_failure / (training_success + training_failure)
     return test_failure_rate, training_failure_rate