8d142cff3a1bc73143cb26aabe32f93c2c58b454
Nathan Cassereau <nathan.cassereau@supelec.fr>
2018-10-17 16:09:32 +0200

    Using numpy functions to be faster :)

--- outils.py
+++ outils.py
@@ -1,24 +1,14 @@
 #! /usr/bin/python3
 # -*- coding:utf-8 -*-
 
+import numpy as np
 import math
 
-hardlim = lambda x : int(x >= 0)
-
-sigmoid = lambda x : 1 / (1 + math.exp(-x))
+sigmoid = lambda x : 1 / (1 + np.exp(-x))
 sigmoid_prim = lambda x : sigmoid(x) * (1 - sigmoid(x))
 
-linear = lambda x : x
-linear_prim = lambda x : 1
-
-def tanh(x) :
-    X = math.exp(2 * x)
-    return (X - 1) / (X + 1)
+tanh=np.tanh
 tanh_prim = lambda x : 1 - tanh(x) ** 2
-
-reLU = lambda x : x if x >= 0 else 0
-reLU_prim = lambda x : 1 if x >=0 else 0
-
 
 def create_range(start,end,step) :
     # Can create range with non integer but float
