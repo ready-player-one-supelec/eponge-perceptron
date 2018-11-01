e8155e7006167fc36642f879d28206cfab75896e
Paul Le Grand Des Cloizeaux <paul.legranddec@gmail.com>
2018-11-01 01:31:35 +0100

    added the tool to red idx to outils

--- outils.py
+++ outils.py
@@ -3,21 +3,39 @@
 
 import numpy as np
 import math
+import struct
 
-sigmoid = lambda x : 1 / (1 + np.exp(-x))
-sigmoid_prim = lambda x : sigmoid(x) * (1 - sigmoid(x))
 
-tanh=np.tanh
-tanh_prim = lambda x : 1 - tanh(x) ** 2
+def sigmoid(x): return 1 / (1 + np.exp(-x))
 
-def create_range(start,end,step) :
+
+def sigmoid_prim(x): return sigmoid(x) * (1 - sigmoid(x))
+
+
+tanh = np.tanh
+
+
+def tanh_prim(x): return 1 - tanh(x) ** 2
+
+
+def create_range(start, end, step):
     # Can create range with non integer but float
     # Be careful, end is included unlike classical range functions
     arrondi = int(-math.log10(step))
-    if arrondi < 1 :
+    if arrondi < 1:
         return [start + i * step for i in range(int((end-start) // step) + 1)]
-    else :
+    else:
         return [round(start+i*step, arrondi) for i in range(int((end-start) // step) + 1)]
 
-regulated_tanh = lambda x : 1.7159 * tanh(2 * x / 3)
-regulated_tanh_prim = lambda x : 1.7159 * (2 / 3) * tanh_prim(2 * x / 3)
+
+def regulated_tanh(x): return 1.7159 * tanh(2 * x / 3)
+
+
+def regulated_tanh_prim(x): return 1.7159 * (2 / 3) * tanh_prim(2 * x / 3)
+
+
+def read_idx(filename):
+    with open(filename, 'rb') as f:
+        zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        return np.fromstring(f.read(), dtype=np.uint8).reshape(shape).astype(np.float_)
